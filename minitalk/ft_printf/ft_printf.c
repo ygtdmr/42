85979d6e9bc5b4179d1fb3443a4a860e305e0158
Yiğit Demir <yidemir@k1m39s02.42istanbul.com.tr>
2025-01-16 17:18:58 +0300

    feat(minitalk): implement project functionality

---
+++ minitalk/ft_printf/ft_printf.c
@@ -0,0 +1,38 @@
+/* ************************************************************************** */
+/*                                                                            */
+/*                                                        :::      ::::::::   */
+/*   ft_printf.c                                        :+:      :+:    :+:   */
+/*                                                    +:+ +:+         +:+     */
+/*   By: yidemir <yidemir@student.42.fr>            +#+  +:+       +#+        */
+/*                                                +#+#+#+#+#+   +#+           */
+/*   Created: 2024/11/19 22:09:27 by yidemir           #+#    #+#             */
+/*   Updated: 2024/12/04 19:30:10 by yidemir          ###   ########.fr       */
+/*                                                                            */
+/* ************************************************************************** */
+
+#include "ft_printf.h"
+
+int	ft_printf(const char *f, ...)
+{
+	va_list	args;
+	int		l;
+	int		eorl;
+
+	l = 0;
+	va_start(args, f);
+	while (*f)
+	{
+		if (*f == '%')
+		{
+			eorl = ft_putf(f, args);
+			f += 2;
+		}
+		else
+			eorl = ft_putchr(*f++);
+		if (eorl == -1)
+			return (va_end(args), -1);
+		else
+			l += eorl;
+	}
+	return (va_end(args), l);
+}
---
+++ minitalk/ft_printf/ft_strncmp.c
@@ -0,0 +1,36 @@
+/* ************************************************************************** */
+/*                                                                            */
+/*                                                        :::      ::::::::   */
+/*   ft_strncmp.c                                       :+:      :+:    :+:   */
+/*                                                    +:+ +:+         +:+     */
+/*   By: yidemir <yidemir@student.42.fr>            +#+  +:+       +#+        */
+/*                                                +#+#+#+#+#+   +#+           */
+/*   Created: 2024/10/24 18:25:19 by yidemir           #+#    #+#             */
+/*   Updated: 2024/11/30 14:04:24 by yidemir          ###   ########.fr       */
+/*                                                                            */
+/* ************************************************************************** */
+
+#include "ft_printf.h"
+
+int	ft_strncmp(const char *s1, const char *s2, int n)
+{
+	unsigned char	c1;
+	unsigned char	c2;
+
+	while (n--)
+	{
+		c1 = *s1++;
+		c2 = *s2++;
+		if (!c1 && !c2)
+			break ;
+		if (!c1)
+			return (-1);
+		if (!c2)
+			return (1);
+		if (c1 > c2)
+			return (1);
+		if (c1 < c2)
+			return (-1);
+	}
+	return (0);
+}
