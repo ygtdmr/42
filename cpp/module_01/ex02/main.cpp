af28b22e5c8c3f23830b64a25d28405fcf762fe3
yigit <ygt.dmr.social@gmail.com>
2025-10-06 18:06:53 -0400

    chore(cpp-module-01): remove generated artifacts

--- cpp/module_01/ex02/main.cpp
+++ cpp/module_01/ex02/main.cpp
@@ -6,7 +6,7 @@
 /*   By: yidemir <yidemir@student.42.fr>            +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2025/10/06 17:47:09 by yidemir           #+#    #+#             */
-/*   Updated: 2025/10/06 17:55:46 by yidemir          ###   ########.fr       */
+/*   Updated: 2025/10/06 18:01:33 by yidemir          ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -20,11 +20,11 @@ int	main( void )
 	std::string	&stringREF(string);
 
 	string = "HI THIS IS BRAIN";
-	std::cout << "Address of str: " << &string << std::endl;
-	std::cout << "Address in stringPTR: " << stringPTR << std::endl;
-	std::cout << "Address in stringREF: " << &stringREF << std::endl;
-	std::cout << "Value of str: " << string << std::endl;
-	std::cout << "Value pointed by PTR: " << *stringPTR << std::endl;
-	std::cout << "Value from REF: " << stringREF << std::endl;
+	std::cout << "The memory address of the string variable: " << &string << std::endl;
+	std::cout << "The memory address held by stringPTR: " << stringPTR << std::endl;
+	std::cout << "The memory address held by stringREF: " << &stringREF << std::endl;
+	std::cout << "The value of the string variable: " << string << std::endl;
+	std::cout << "The value pointed to by stringPTR: " << *stringPTR << std::endl;
+	std::cout << "The value pointed to by stringREF: " << stringREF << std::endl;
 	return (0);
 }
