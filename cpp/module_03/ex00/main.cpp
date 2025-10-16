d17de0926f66db99c8719b57b2d8f36c680196fc
Yiğit Demir <yidemir@k1m01s06.42istanbul.com.tr>
2025-10-16 12:51:39 +0300

    refactor(cpp-module-03): simplify inheritance-based combat classes

--- cpp/module_03/ex00/main.cpp
+++ cpp/module_03/ex00/main.cpp
@@ -6,7 +6,7 @@
 /*   By: yidemir <yidemir@student.42.fr>            +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2025/10/13 04:57:13 by yidemir           #+#    #+#             */
-/*   Updated: 2025/10/16 12:04:05 by yidemir          ###   ########.fr       */
+/*   Updated: 2025/10/16 12:38:08 by yidemir          ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -15,29 +15,17 @@
 
 int main( void )
 {
-    ClapTrap a("Bob");
-
-    std::cout << std::endl;
+    ClapTrap	a("Bob");
 
     a.attack("TargetDummy");
-    std::cout << std::endl;
-
     a.takeDamage(3);
-    std::cout << std::endl;
-
     a.beRepaired(5);
-    std::cout << std::endl;
 
     for (int i = 0; i < 10; i++)
         a.attack("TrainingBot");
-    std::cout << std::endl;
 
     a.takeDamage(20);
-    std::cout << std::endl;
-
     a.attack("Nothing");
     a.beRepaired(10);
-    std::cout << std::endl;
-
 	return (0);
 }
