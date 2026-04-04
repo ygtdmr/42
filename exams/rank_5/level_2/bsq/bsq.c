0daa391e541d1677019bc85ec507ee9fba74ecb4
ygtdmr <ygt.dmr.social@gmail.com>
2026-04-04 19:44:33 +0000

    feat(repo): add exam exercise solutions

--- exams/rank_5/level_2/bsq/bsq.c
+++ exams/rank_5/level_2/bsq/bsq.c
@@ -1,14 +1,19 @@
 #include <stdlib.h>
 #include <stdio.h>
-#include <ctype.h>
-#include <string.h>
 
-typedef struct	s_axis
+size_t	ft_strlen(char *s);
+char	*ft_strdup(char *s);
+void	ft_bzero(void *p, size_t size);
+int		ft_atoi(char *s);
+int		ft_isdigit(char c);
+int		ft_isprint(char c);
+
+typedef struct	s_cube
 {
 	size_t	x;
 	size_t	y;
 	size_t	size;
-}	t_axis;
+}	t_cube;
 
 typedef struct	s_bsq
 {
@@ -16,6 +21,7 @@ typedef struct	s_bsq
 	char	o;
 	char	f;
 	size_t	size;
+	size_t	line_size;
 	char	**map;
 	int		**map_d;
 }	t_bsq;
@@ -34,7 +40,7 @@ void	process(t_bsq	*bsq)
 {
 	size_t	x;
 	size_t	y;
-	t_axis	max;
+	t_cube	max;
 
 	y = 0;
 	max.size = 0;
@@ -85,15 +91,15 @@ int	parse_header(char *l, t_bsq *bsq)
 	if (!l)
 		return (0);
 	sdl = 0;
-	while(isdigit(l[sdl]))
+	while(ft_isdigit(l[sdl]))
 		sdl++;
 	if (!sdl)
 		return (0);
-	bsq->size = atoi(l);
+	bsq->size = ft_atoi(l);
 	l += sdl;
-	if (!(strlen(l) == 4 && l[3] == '\n'))
+	if (!(ft_strlen(l) == 4 && l[3] == '\n'))
 		return (0);
-	if (!(isprint(l[0]) && isprint(l[1]) && isprint(l[2])))
+	if (!(ft_isprint(l[0]) && ft_isprint(l[1]) && ft_isprint(l[2])))
 		return (0);
 	if (!(l[0] != l[1] && l[0] != l[2] && l[1] != l[2]))
 		return (0);
@@ -106,15 +112,16 @@ int	parse_header(char *l, t_bsq *bsq)
 int	verify_line(char *l, t_bsq *bsq)
 {
 	char			*nlp;
-	static size_t	prev_length;
 	size_t			length;
 
-	length = strlen(l);
-	nlp = (l + (length - 1));
-	if (!prev_length)
-		prev_length = length;
-	if (!(l && prev_length == length))
+	if (!l)
 		return (0);
+	length = ft_strlen(l);
+	if (!bsq->line_size)
+		bsq->line_size = length;
+	if (bsq->line_size != length)
+		return (0);
+	nlp = (l + (length - 1));
 	while(l != nlp)
 	{
 		if (!((*l == bsq->e) || (*l == bsq->o) || (*l == bsq->f)))
@@ -162,8 +169,8 @@ int	parse(FILE *fp, t_bsq *bsq)
 	{
 		if (!(size <= bsq->size && verify_line(line, bsq)))
 			return (safe_free(line, 0));
-		bsq->map[size] = strdup(line);
-		bsq->map_d[size] = calloc(strlen(line), sizeof(int));
+		bsq->map[size] = ft_strdup(line);
+		bsq->map_d[size] = calloc(ft_strlen(line), sizeof(int));
 		size++;
 	}
 	return (safe_free(line, size == (bsq->size)));
@@ -173,7 +180,7 @@ void	run(char *path, t_bsq *bsq)
 {
 	FILE	*fp;
 
-	bzero(bsq, sizeof(t_bsq));
+	ft_bzero(bsq, sizeof(t_bsq));
 	if (path)
 		fp = fopen(path, "r");
 	else
@@ -194,8 +201,13 @@ int	main(int argc, char **argv)
 
 	if (argc > 1)
 	{
-		while(*(++argv))
-			run(*argv, &bsq);
+		argv++;
+		while(*argv)
+		{
+			run(*argv++, &bsq);
+			if (*argv)
+				fputs("\n", stdout);
+		}
 	}
 	else
 		run(0, &bsq);
