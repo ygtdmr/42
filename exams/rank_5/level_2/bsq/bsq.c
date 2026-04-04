6d57d42abed027f64e748e3c6d38b83431b81896
Yiğit Demir <yidemir@k1m02s08.42istanbul.com.tr>
2026-04-04 18:28:58 +0300

    feat(repo): add exam exercise solutions

--- exams/rank_5/level_2/bsq/bsq.c
+++ exams/rank_5/level_2/bsq/bsq.c
@@ -1,184 +1,203 @@
-#include <unistd.h>
 #include <stdlib.h>
 #include <stdio.h>
+#include <ctype.h>
+#include <string.h>
+
+typedef struct	s_axis
+{
+	size_t	x;
+	size_t	y;
+	size_t	size;
+}	t_axis;
 
 typedef struct	s_bsq
-{
-	char	**map;
-	char	empty;
-	char	obstacle;
-	char	full;
-	size_t	len;
-}	t_bsq;
-
-void	put_err(void)
-{
-	fprintf(stderr, "map error\n");
-}
-
-size_t	ft_strlen(char *s)
-{
-	size_t	l;
-
-	l = 0;
-	while(s && s[l])
-		l++;
-	return (l);
-}
-
-char	*ft_strdup(char *s)
-{
-	char	*dup;
-	size_t	i_dup;
-	size_t	len;
-
-	i_dup = 0;
-	len = ft_strlen(s);
-	dup = calloc((len + 1), sizeof(char));
-	while(i_dup < len)
-	{
-		dup[i_dup] = s[i_dup];
-		i_dup++;
-	}
-	return (dup);
-}
-
-int	verify_line(t_bsq *bsq, char *line)
 {
 	char	e;
 	char	o;
 	char	f;
-	char	*nl;
+	size_t	size;
+	char	**map;
+	int		**map_d;
+}	t_bsq;
 
-	e = bsq->empty;
-	o = bsq->obstacle;
-	f = bsq->full;
-	nl = line + ft_strlen(line) - 1;
-	while (line && *line)
-	{
-		if ((line != nl) && (*line != e) && (*line != o) && (*line != f))
-			return (0);
-		line++;
-	}
-	return (1);
-}
-
-int	parse_config(t_bsq *bsq, char *line)
+int min(int x, int y, int z)
 {
-	int	i;
+    int m = x;
+    if (y < m)
+		m = y;
+    if (z < m)
+		m = z;
+    return (m);
+}
 
-	i = 0;
-	if (ft_strlen(line) != 5)
-		return (0);
-	while(line[i])
+void	process(t_bsq	*bsq)
+{
+	size_t	x;
+	size_t	y;
+	t_axis	max;
+
+	y = 0;
+	max.size = 0;
+	while(y < bsq->size)
 	{
-		if (i == 0)
+		x = 0;
+		while(bsq->map[y][x])
 		{
-			if(line[i] >= '0' && line[i] <= '9')
-				bsq->len = line[i] - '0';
+			if (bsq->map[y][x] == bsq->o)
+				bsq->map_d[y][x] = 0;
+			else if(!y || !x)
+				bsq->map_d[y][x] = 1;
 			else
-				return (0);
+				bsq->map_d[y][x] = min(bsq->map_d[y][x - 1], bsq->map_d[y - 1][x], bsq->map_d[y - 1][x - 1]) + 1;
+			if (bsq->map_d[y][x] > max.size)
+			{
+				max.size = bsq->map_d[y][x];
+				max.x = x;
+				max.y = y;
+			}
+			x++;
 		}
-		else if(i == 1)
-			bsq->empty = line[i];
-		else if(i == 2)
-			bsq->obstacle = line[i];
-		else if(i == 3)
-			bsq->full = line[i];
-		i++;
+		y++;
+	}
+	y = 0;
+	while(y < max.size)
+	{
+		x = 0;
+		while(x < max.size)
+		{
+			bsq->map[max.y - y][max.x - x] = bsq->f;
+			x++;
+		}
+		y++;
+	}
+	y = 0;
+	while(y < bsq->size)
+	{
+		fputs(bsq->map[y], stdout);
+		y++;
+	}
+}
+
+int	parse_header(char *l, t_bsq *bsq)
+{
+	size_t	sdl;
+
+	if (!l)
+		return (0);
+	sdl = 0;
+	while(isdigit(l[sdl]))
+		sdl++;
+	if (!sdl)
+		return (0);
+	bsq->size = atoi(l);
+	l += sdl;
+	if (!(strlen(l) == 4 && l[3] == '\n'))
+		return (0);
+	if (!(isprint(l[0]) && isprint(l[1]) && isprint(l[2])))
+		return (0);
+	if (!(l[0] != l[1] && l[0] != l[2] && l[1] != l[2]))
+		return (0);
+	bsq->e = l[0];
+	bsq->o = l[1];
+	bsq->f = l[2];
+	return (1);
+}
+
+int	verify_line(char *l, t_bsq *bsq)
+{
+	char			*nlp;
+	static size_t	prev_length;
+	size_t			length;
+
+	length = strlen(l);
+	nlp = (l + (length - 1));
+	if (!prev_length)
+		prev_length = length;
+	if (!(l && prev_length == length))
+		return (0);
+	while(l != nlp)
+	{
+		if (!((*l == bsq->e) || (*l == bsq->o) || (*l == bsq->f)))
+			return (0);
+		l++;
 	}
 	return (1);
 }
 
-int	parse(t_bsq *bsq, FILE *fp)
+int	safe_free(void *p, int status)
+{
+	if (p)
+		free(p);
+	return (status);
+}
+
+void	free_map(void **map, size_t size)
+{
+	size_t	i;
+
+	if (!map)
+		return ;
+	i = 0;
+	while(i < size)
+		free(map[i++]);
+	free(map);
+}
+
+int	parse(FILE *fp, t_bsq *bsq)
 {
 	char	*line;
-	size_t	buf_len;
-	ssize_t	tmp_len;
-	size_t	i_map;
-	size_t	l_map;
+	size_t	n;
+	size_t	size;
 
-	buf_len = 0;
-	line = 0;
 	if (!fp)
 		return (0);
-	tmp_len = getline(&line, &buf_len, fp);
-	if (!((tmp_len != -1) && parse_config(bsq, line)))
+	n = 0;
+	size = 0;
+	line = 0;
+	if (!((getline(&line, &n, fp) != -1) && parse_header(line, bsq)))
+		return (safe_free(line, 0));
+	bsq->map = calloc(bsq->size, sizeof(char *));
+	bsq->map_d = calloc(bsq->size, sizeof(int *));
+	while(getline(&line, &n, fp) != -1)
 	{
-		free(line);
-		return (0);
+		if (!(size <= bsq->size && verify_line(line, bsq)))
+			return (safe_free(line, 0));
+		bsq->map[size] = strdup(line);
+		bsq->map_d[size] = calloc(strlen(line), sizeof(int));
+		size++;
 	}
-	bsq->map = calloc((bsq->len + 1), sizeof(char *));
-	tmp_len = -1;
-	i_map = 0;
-	l_map = 0;
-	while(getline(&line, &buf_len, fp) != -1)
-	{
-		if (tmp_len == -1)
-			tmp_len = ft_strlen(line);
-		if (tmp_len != ft_strlen(line))
-			break;
-		if (verify_line(bsq, line))
-			bsq->map[i_map++] = ft_strdup(line);
-		else
-			break;
-		l_map++;
-	}
-	free(line);
-	return ( bsq->len == l_map );
+	return (safe_free(line, size == (bsq->size)));
 }
 
-void	clean(t_bsq *bsq)
+void	run(char *path, t_bsq *bsq)
 {
-	size_t	i;
+	FILE	*fp;
 
-	if (!bsq->map)
-		return; 
-	i = 0;
-	while(bsq->map[i])
-		free(bsq->map[i++]);
-	free(bsq->map);
-	bsq->map = 0;
-}
-
-void	run(t_bsq *bsq)
-{
-	size_t	i;
-
-	i = 0;
-	fprintf(stdout, "config: l=%ld e=|%c| o=|%c| f=|%c|\n", bsq->len, bsq->empty, bsq->obstacle, bsq->full);
-	while (bsq->map[i])
-		fprintf(stdout, "%s", bsq->map[i++]);
+	bzero(bsq, sizeof(t_bsq));
+	if (path)
+		fp = fopen(path, "r");
+	else
+		fp = stdin;
+	if (parse(fp, bsq))
+		process(bsq);
+	else
+		fprintf(stderr, "error\n");
+	if (fp && (fp != stdin))
+		fclose(fp);
+	free_map((void **) bsq->map, bsq->size);
+	free_map((void **) bsq->map_d, bsq->size);
 }
 
 int	main(int argc, char **argv)
 {
-	FILE	*fp;
 	t_bsq	bsq;
-	char	*line;
 
-	bsq.map = 0;
-	if (argc < 2)
+	if (argc > 1)
 	{
-		if (parse(&bsq, stdin))
-			run(&bsq);
-		else
-			put_err();
-		clean(&bsq);
+		while(*(++argv))
+			run(*argv, &bsq);
 	}
 	else
-	{
-		while (*(++argv))
-		{
-			fp = fopen(*argv, "r");
-			if (parse(&bsq, fp))
-				run(&bsq);
-			else
-				put_err();
-			clean(&bsq);
-			if (fp)
-				fclose(fp);
-		}
-	}
+		run(0, &bsq);
 	return (0);
 }
