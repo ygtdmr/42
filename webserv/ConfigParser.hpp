aa18faaf63fc86c193534ae2f4cda98e817b8b74
Yiğit Demir <yidemir@student.42istanbul.com.tr>
2026-06-15 19:20:51 +0300

    feat(webserv): add server configuration parsing

--- webserv/ConfigParser.hpp
+++ webserv/ConfigParser.hpp
@@ -6,13 +6,17 @@
 /*   By: yidemir <yidemir@student.42.fr>            +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2026/06/14 19:54:57 by yidemir           #+#    #+#             */
-/*   Updated: 2026/06/14 21:04:51 by yidemir          ###   ########.fr       */
+/*   Updated: 2026/06/15 18:57:10 by yidemir          ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
-#include <fstream>
-#include <string>
 #include "ServerConfig.hpp"
+
+struct ConfigParserToken
+{
+	std::vector<std::string>	words;
+	ConfigParserToken			*child;
+};
 
 class ConfigParser
 {
@@ -20,10 +24,13 @@ class ConfigParser
 		ConfigParser( std::string const& path );
 		ConfigParser( ConfigParser const& other );
 		~ConfigParser();
-		ConfigParser	&operator=( ConfigParser const& other );
-		ServerConfig	*getServerConfig( void );
-		void			parse( void );
+		ConfigParser				&operator=( ConfigParser const& other );
+		std::vector<ServerConfig>	*getServers( void ) const;
+		void						parse( void );
 	private:
-		std::string		path_;
-		ServerConfig	*serverConfig_;
+		void						throwError( std::string const& msg ) const;
+		ConfigParserToken			*getToken( std::ifstream& ifs ) const;
+
+		std::string					path_;
+		std::vector<ServerConfig>	*servers_;
 };
--- webserv/main.cpp
+++ webserv/main.cpp
@@ -6,7 +6,7 @@
 /*   By: yidemir <yidemir@student.42.fr>            +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2026/06/14 19:58:59 by yidemir           #+#    #+#             */
-/*   Updated: 2026/06/14 20:23:02 by yidemir          ###   ########.fr       */
+/*   Updated: 2026/06/15 14:13:22 by yidemir          ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -18,13 +18,17 @@ int	main( int argc, char **argv )
 	if ( argc != 2 )
 	{
 		std::cout
-			<< "usage: ./webserv [configuration file]";
+			<< "usage: ./webserv [configuration file]"
+			<< std::endl;
 		return ( 1 );
 	}
 	try
 	{
 		ConfigParser	configParser(argv[1]);
+		std::vector<ServerConfig>	*configServers;
+
 		configParser.parse();
+		configServers = configParser.getServers();
 	}
 	catch( const std::exception& e )
 	{
