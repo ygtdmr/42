22b2fcb5c0254755a1d542c81e0b43eb030b1dbf
ygtdmr <ygt.dmr.social@gmail.com>
2026-07-10 21:02:30 +0000

    feat(webserv): add HTTP response handling

--- webserv/test/debug.hpp
+++
@@ -1,6 +0,0 @@
-#include <iostream>
-#include "../include/ServerConfig.hpp"
-
-void printSevers( std::vector<ServerConfig> servers );
-
-std::ostream	&operator<<( std::ostream &os, ServerConfig const &sc );
